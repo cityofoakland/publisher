732d13846b7c99e3e6b816a85014bda11c1afd9b
James Stewart <jys@ketlai.co.uk>
2012-08-15 09:10:20 +0100

    Move tests towards more ruby 1.9 hash syntax

--- test/functional/admin/editions_controller_test.rb
+++ test/functional/admin/editions_controller_test.rb
@@ -16,12 +16,12 @@ class Admin::EditionsControllerTest < ActionController::TestCase
         owning_app: "publisher")
     @programme = ProgrammeEdition.create(title: "test", slug: "test2", panopticon_id: artefact2.id)
 
-    stub_request(:delete, "#{Plek.current.find("arbiter")}/slugs/test").to_return(:status => 200)
+    stub_request(:delete, "#{Plek.current.find("arbiter")}/slugs/test").to_return(status: 200)
   end
 
   test "it renders the lgsl edit form successfully if creation fails" do
     lgsl_code = 800
-    local_service = FactoryGirl.create(:local_service, :lgsl_code=>lgsl_code)
+    local_service = FactoryGirl.create(:local_service, lgsl_code: lgsl_code)
 
     post :create, "edition" => {"kind" => "local_transaction", "lgsl_code"=>lgsl_code, "panopticon_id"=>"827", "title"=>"a title"}
     assert_equal '302', response.code
@@ -39,7 +39,7 @@ class Admin::EditionsControllerTest < ActionController::TestCase
 
   test "an appropriate error message is shown if new edition failed" do
     @user.stubs(:new_version).with(@guide, nil).returns(false)
-    post :duplicate, :id => @guide.id
+    post :duplicate, id: @guide.id
     assert_response 302
     assert_equal "Failed to create new edition: couldn't initialise", flash[:alert]
   end
@@ -48,15 +48,15 @@ class Admin::EditionsControllerTest < ActionController::TestCase
     @guide.update_attribute(:state, :ready)
 
     post :progress,
-      :id       => @guide.id,
-      :activity => {
+      id:       @guide.id,
+      activity: {
         "request_type"       => "send_fact_check",
         "comment"            => "Blah",
         "email_addresses"    => "user@example.com",
         "customised_message" => "Hello"
       }
 
-    assert_redirected_to :controller => "admin/editions", :action => "show", :id => @guide.id
+    assert_redirected_to controller: "admin/editions", action:  "show", id: @guide.id
     assert_equal "Guide updated", flash[:notice]
 
     reloaded = Edition.find(@guide.id)
@@ -68,10 +68,10 @@ class Admin::EditionsControllerTest < ActionController::TestCase
 
     @guide.state = 'ready'
     stub_register_published_content
-    User.create(:name => 'test').publish(@guide, comment: "Publishing this")
+    User.create(name: 'test').publish(@guide, comment: "Publishing this")
 
-    post :duplicate, :id => @guide.id,
-      :edition  => { :assigned_to_id => bob.id, :kind => 'guide' }
+    post :duplicate, id: @guide.id,
+      :edition  => { assigned_to_id: bob.id, :kind => 'guide' }
 
     @new_guide = Edition.where(panopticon_id: @guide.panopticon_id).last
     assert_equal bob, @new_guide.assigned_to
@@ -83,8 +83,8 @@ class Admin::EditionsControllerTest < ActionController::TestCase
     @bob.publish(@guide, comment: "Publishing this")
 
     post :duplicate,
-      :id => @guide.id,
-      :edition  => { :assigned_to_id => "", :kind => 'guide' }
+      id:      @guide.id,
+      edition: { assigned_to_id: "", :kind => 'guide' }
 
     @guide.reload
     assert_nil @guide.assigned_to
@@ -94,8 +94,8 @@ class Admin::EditionsControllerTest < ActionController::TestCase
     bob = User.create
 
     post :update,
-      :id       => @guide.id,
-      :edition  => { :assigned_to_id => bob.id }
+      id:      @guide.id,
+      edition: { assigned_to_id: bob.id }
 
     @guide.reload
     assert_equal bob, @guide.assigned_to
@@ -106,8 +106,8 @@ class Admin::EditionsControllerTest < ActionController::TestCase
     @user.assign(@guide, bob)
 
     post :update,
-      :id       => @guide.id,
-      :edition  => { :assigned_to_id => bob.id }
+      id:      @guide.id,
+      edition: { assigned_to_id: bob.id }
 
     @guide.reload
     assert_equal 1, @guide.actions.select { |a| a.request_type == Action::ASSIGN }.length
@@ -118,8 +118,8 @@ class Admin::EditionsControllerTest < ActionController::TestCase
     @user.assign(@guide, bob)
 
     post :update,
-      :id       => @guide.id,
-      :edition  => { :assigned_to_id => "" }
+      id:      @guide.id,
+      edition: { assigned_to_id: "" }
 
     @guide.reload
     assert_equal bob, @guide.assigned_to
@@ -132,25 +132,25 @@ class Admin::EditionsControllerTest < ActionController::TestCase
 
     Edition.expects(:find).returns(@guide)
     @guide.stubs(:update_attributes).returns(false)
-    @guide.expects(:errors).at_least_once.returns({:title => ['values']})
+    @guide.expects(:errors).at_least_once.returns({title: ['values']})
 
     post :update,
-      :id       => @guide.id,
-      :edition  => { :assigned_to_id => "" }
+      id:      @guide.id,
+      edition: { assigned_to_id: "" }
     assert_response 200
   end
 
   test "should not progress to fact check if the email addresses were blank" do
     post :progress, {
-      :id       => @guide.id.to_s,
-      :activity => { 'request_type' => "send_fact_check" }
+      id:       @guide.id.to_s,
+      activity: { 'request_type' => "send_fact_check" }
     }
     assert_equal "Couldn't send fact check for guide", flash[:alert]
   end
 
   test "should show the edit page after starting work" do
     post :progress, { id: @guide.id.to_s, activity: { "request_type" => 'start_work' } }
-    assert_redirected_to :controller => "admin/editions", :action => "show", :id => @guide.id
+    assert_redirected_to controller: "admin/editions", action:  "show", id: @guide.id
 
     @guide.reload
     assert !@guide.lined_up?
@@ -160,9 +160,9 @@ class Admin::EditionsControllerTest < ActionController::TestCase
   test "destroy transaction" do
     assert @transaction.can_destroy?
     assert_difference('TransactionEdition.count', -1) do
-      delete :destroy, :id => @transaction.id
+      delete :destroy, id: @transaction.id
     end
-    assert_redirected_to(:controller => "root", "action" => "index")
+    assert_redirected_to(controller: "root", action: "index")
   end
 
   test "can't destroy published transaction" do
@@ -172,23 +172,23 @@ class Admin::EditionsControllerTest < ActionController::TestCase
     assert !@transaction.can_destroy?
     @transaction.save!
     assert_difference('TransactionEdition.count', 0) do
-      delete :destroy, :id => @transaction.id
+      delete :destroy, id: @transaction.id
     end
   end
 
   test "editions index redirects to root" do
     get :index
     assert_response :redirect
-    assert_redirected_to(:controller => "root", "action" => "index")
+    assert_redirected_to(controller: "root", action: "index")
   end
 
   test "requesting a publication that doesn't exist returns a 404" do
-    get :show, :id => '4e663834e2ba80480a0000e6'
+    get :show, id: '4e663834e2ba80480a0000e6'
     assert_response 404
   end
 
   test "we can view a programme" do
-    get :show, :id => @programme.id
+    get :show, id: @programme.id
     assert_response :success
     assert_not_nil assigns(:resource)
   end
@@ -196,9 +196,9 @@ class Admin::EditionsControllerTest < ActionController::TestCase
   test "destroy programme" do
     assert @programme.can_destroy?
     assert_difference('ProgrammeEdition.count', -1) do
-      delete :destroy, :id => @programme.id
+      delete :destroy, id: @programme.id
     end
-    assert_redirected_to(:controller => "root", "action" => "index")
+    assert_redirected_to(controller: "root", action: "index")
   end
 
   test "can't destroy published programme" do
@@ -211,7 +211,7 @@ class Admin::EditionsControllerTest < ActionController::TestCase
     assert !@programme.can_destroy?
 
     assert_difference('ProgrammeEdition.count', 0) do
-      delete :destroy, :id => @programme.id
+      delete :destroy, id: @programme.id
     end
   end
 end
